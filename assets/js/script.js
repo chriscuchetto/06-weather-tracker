4ab8716e50474cae8043586924796865e97e53b5
chriscuchetto <chriscuchetto@gmail.com>
2022-12-05 22:23:46 -0600

    added currentWeather and fiveDayForecast divs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
 //Global Variables
-let baseUrl = " https://api.openweathermap.org";
-
+let baseUrl = "https://api.openweathermap.org/data/2.5/forecast?";
+let apiKey = "67282b5715a921433482ffda726b0a94";
+let searchBtn = document.querySelector("#searchBtn");
+let searchBar = document.querySelector("#searchBar");
 
 //Functions
-
-
-
+function init() {
+    fetch("https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=67282b5715a921433482ffda726b0a94")
+        .then(function (response) {
+            return response.json();
+        })
+    }
 //Event Listeners
+init();
